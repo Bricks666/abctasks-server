f58abf06eadad7610e70f3c4ec63fc93b4f1dda1
Bricks666 <tconckoff82@gmail.com>
2024-01-20 23:40:31 +0300

    fix activation requirements

--- src/shared/lib/cookie.decorator.ts
+++ src/shared/lib/cookie.decorator.ts
@@ -5,8 +5,6 @@ export const Cookie = createParamDecorator<string, ExecutionContext>(
 	(cookie, context) => {
 		const req: Request = context.switchToHttp().getRequest();
 
-		console.log(req.cookies);
-
 		return req.cookies[cookie] ?? null;
 	}
 );
